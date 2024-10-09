272a2252885c974593134fcbc7aa4ae4f581f02a
DESKTOP-9S4LV2D\gywns <gywns7967@gmail.com>
2024-10-10 03:07:29 +0900

    [수정] 플레이리스트,  댓글 mapper, 서비스 연결

--- src/main/java/com/example/musicemotion/comment/service/CommentService.java
+++ src/main/java/com/example/musicemotion/comment/service/CommentService.java
@@ -4,9 +4,28 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.musicemotion.dto.CommentsDTO;
+
+
 @Service
 public class CommentService {
 
 	@Autowired
 	private  SqlSession sqlSession;
+	
+	 public void addComments(CommentsDTO dto) {
+		 sqlSession.insert("addComments", dto);
+	 }
+	 
+	 public CommentsDTO commentsAll(String post_id) {
+		 return  sqlSession.selectOne("commentsAll", post_id);
+	 }
+	 
+	 public void updateComments(CommentsDTO dto) {
+		 sqlSession.update("updateComments", dto);
+	 }
+	 
+	 public void deleteComments(int comment_id) {
+		 sqlSession.delete("deleteComments",comment_id);
+	 }
 }
--- src/main/java/com/example/musicemotion/playList/service/PlaylistService.java
+++ src/main/java/com/example/musicemotion/playList/service/PlaylistService.java
@@ -4,9 +4,29 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.musicemotion.dto.PlaylistDTO;
+
+
+
 @Service
 public class PlaylistService {
 
 	@Autowired
 	private  SqlSession sqlSession;
+	
+	 public void addPlaylist(PlaylistDTO dto) {
+		 sqlSession.insert("addPlaylist", dto);
+	 }
+	 
+	 public PlaylistDTO playlistAll(String user_id) {
+		 return  sqlSession.selectOne("playlistAll", user_id);
+	 }
+	 
+	 public void updatePlaylist(PlaylistDTO dto) {
+		 sqlSession.update("updatePlaylist", dto);
+	 }
+	 
+	 public void deletePlaylist(int playlist_id) {
+		 sqlSession.delete("deletePlaylist",playlist_id);
+	 }
 }
