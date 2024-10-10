855f372cd29b80d3c3cc0bb8cd41aae4d9829b3c
DESKTOP-9S4LV2D\gywns <gywns7967@gmail.com>
2024-10-11 04:15:05 +0900

    [수정] 서비스 제작 중

--- src/main/java/com/example/musicemotion/album/service/AlbumService.java
+++ src/main/java/com/example/musicemotion/album/service/AlbumService.java
@@ -4,9 +4,29 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.musicemotion.dto.AlbumDTO;
+
+
+
 @Service
 public class AlbumService {
 
 	@Autowired
 	private  SqlSession sqlSession;
+	
+	 public void addAlbum(AlbumDTO dto) {
+		 sqlSession.insert("addAlbum", dto);
+	 }
+	 
+	 public AlbumDTO albumAll(String user_id) {
+		 return  sqlSession.selectOne("albumAll", user_id);
+	 }
+	 
+	 public void updateAlbum(AlbumDTO dto) {
+		 sqlSession.update("updateAlbum", dto);
+	 }
+	 
+	 public void deleteAlbum(String album_id) {
+		 sqlSession.delete("deleteAlbum",album_id);
+	 }
 }
--- src/main/java/com/example/musicemotion/artist/service/ArtistService.java
+++ src/main/java/com/example/musicemotion/artist/service/ArtistService.java
@@ -4,9 +4,28 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.musicemotion.dto.ArtistDTO;
+
+
 @Service
 public class ArtistService {
 
 	@Autowired
 	private  SqlSession sqlSession;
+	
+	 public void addArtist(ArtistDTO dto) {
+		 sqlSession.insert("addArtist", dto);
+	 }
+	 
+	 public ArtistDTO artistAll(String user_id) {
+		 return  sqlSession.selectOne("artistAll", user_id);
+	 }
+	 
+	 public void updateArtist(ArtistDTO dto) {
+		 sqlSession.update("updateArtist", dto);
+	 }
+	 
+	 public void deleteAlbum(String artist_id) {
+		 sqlSession.delete("deleteAlbum",artist_id);
+	 }
 }
